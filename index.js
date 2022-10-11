b387776c547fcc444fa0d9f537a77a50247d86e1
parykhan-jameel <parexan.00443431@gmail.com>
2022-10-11 23:51:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainVar = document.getElementById(`main`);
+mainVar.remove();
+
+const newHeader = document.createElement(`h1`);
+newHeader.id = `victory`;
+newHeader.innerHTML = `Anthony is the champion`;
